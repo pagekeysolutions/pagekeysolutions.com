33be4a287ba66d30b1c20939e95b3c646bc28650
Stephen Grice <stephengrice@live.com>
2018-07-15 11:10:33 -0400

    Lay skeleton for new idea

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+class ChoiceArea extends Component {
+  render() {
+    return (
+      <div className="ChoiceArea">
+        <p className="title">
+          Choose a path
+        </p>
+        <div className="choice choice-left">
+          <label>Manage my site</label>
+          <button>&lt;---</button>
+        </div>
+        <div className="choice choice-right">
+          <label>Create something new</label>
+          <button>---&gt;</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="app-header">
+          <img className="logo" src={logo} alt="logo" />
+          <h1 className="title">PageKey Solutions</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main-content">
+          <ChoiceArea />
+        </div>
       </div>
     );
   }
