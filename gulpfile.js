381907db8d109f12e94d7618c32c9958125085d0
Stephen Grice <stephengrice@live.com>
2019-01-20 19:47:01 -0500

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp  = require('gulp');
 var deploy = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
 
 gulp.task('deploy', function() {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		open: 'external',
+	    	//injectChanges: true,
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
