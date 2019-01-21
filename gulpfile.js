feff060f3f053e45445715d2165ed1c40ffb39a4
Stephen Grice <stephengrice@live.com>
2019-01-20 21:39:35 -0500

    Update project structure and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,56 @@
-var gulp  = require('gulp');
+const {series, parallel, src, dest} = require('gulp');
+var sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
+    clean = require('gulp-clean');
+
+function cleanProject() {
+	return src('build', {read:false, allowEmpty: true})
+		.pipe(clean());
+}
+function compileSass() {
+	return src('app/scss/*.scss')
+		.pipe(sass())
+		.pipe(dest('build/css'))
+		.pipe(livereload());
+};
+function build() {
+	return src(["app/*.html", "app/js/*", "app/img/*"], {base: './app'})
+		.pipe(dest('build'))
+		.pipe(livereload());
+}
+exports.clean = series(cleanProject);
+exports.build = series(
+	cleanProject,
+	compileSass,
+	build
+);
+/*
+var concat = require('gulp-concat'),
+    del = require('del'),
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass');
 var deploy = require('gulp-gh-pages');
 var browserSync = require('browser-sync').create();
 
-gulp.task('deploy', function() {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
+gulp.task('compileSass', function() {
+	return gulp.src('scss/main.css')
+		.pipe(sass())
+		.pipe(gulp.dest('css'))
+		.pipe(livereload());
 });
 
+gulp.task('build', ['compileSass'], function() {
+	return gulp.src(["css/*", "img/*", "js/*"], {base: './'})
+		.pipe(gulp.dest('public'))
+		.pipe(livereload());
+});
+*/
+
+//gulp.task('deploy', function() {
+//  return gulp.src("./dist/**/*")
+//    .pipe(deploy())
+//});
+/*
 gulp.task('browser-sync', function() {
 	browserSync.init({
 		open: 'external',
@@ -16,4 +60,4 @@ gulp.task('browser-sync', function() {
 		}
 	});
 });
-
+*/
