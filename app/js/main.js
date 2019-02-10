a116c7e0927e61a2c9931f30945c12f5daa13125
Stephen Grice <stephengrice@live.com>
2019-02-10 15:33:36 -0500

    Tween up to the first bubble in slide 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var controller = new ScrollMagic.Controller();
   // Slide 1
-  var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "75%", offset: "0"});
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "90%", offset: "0"});
   var timeline = new TimelineMax();
   timeline.add(
     TweenMax.to("#scroll-prompt, #pen-key, #swoops", 1, {opacity: 0}), 0
@@ -10,18 +10,22 @@ $(document).ready(function() {
   ).add(
     TweenMax.to("#get-started", 1, {opacity: 1})
   ).add(
-    TweenMax.from("#checks", 1, {top: "100%"})
+    TweenMax.to("#checks", 1, {left: "50%"})
   );
   scene.setTween(timeline);
-  // scene.addIndicators({name: "Slide 1"})  // add indicators (requires plugin)
+  scene.addIndicators({name: "Slide 1"})  // add indicators (requires plugin)
   scene.addTo(controller);
   // Slide 2
-  // scene = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: "50%", offset: "0"});
-  // timeline = new TimelineMax();
+  scene = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: "50%", offset: "0"});
+  timeline = new TimelineMax();
   // tween1 = ;
   // tween2 = TweenMax.to("body", 1, {backgroundColor: "#18633F"});
-  // timeline.add(tween1);
-  // scene.setTween(timeline);
-  // scene.addIndicators({name: "Scene 2"})  // add indicators (requires plugin)
-  // scene.addTo(controller);
+  timeline.add(
+    TweenMax.to("#checks #line2", 1, {marginTop: "100%"})
+  ).add(
+    TweenMax.to("#slide2-bubble", 1, {opacity: 1})
+  );
+  scene.setTween(timeline);
+  scene.addIndicators({name: "Slide 2"})  // add indicators (requires plugin)
+  scene.addTo(controller);
 });
